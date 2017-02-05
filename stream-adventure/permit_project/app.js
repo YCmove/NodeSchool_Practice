1f19ef1bfa8f3a581a622c3c3d4dea5720799b2d
YCmove <y8765gd@gmail.com>
2017-02-06 01:27:35 +0800

    taiwan mountain permit use csvtojson

---
+++ stream-adventure/permit_project/app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const fs = require('fs');
+const csv = require('csvtojson');
+
+let readableStream = fs.createReadStream('./mountain_permits_201701.csv');
+
+readableStream.on('data', chunk => {
+    // console.log(chunk);
+    // toJson(chunk.toString());
+});
+
+readableStream.on('end', () => {
+    console.log('end here');
+});
+
+
+// from string
+function toJson(csvStr) {
+    csv({
+            noheader: true
+        })
+        .fromString(csvStr)
+        .on('csv', (csvRow) => { // this func will be called 3 times 
+            console.log(csvRow) // => [1,2,3] , [4,5,6]  , [7,8,9] 
+        })
+        .on('done', () => {
+            //parsing finished 
+            console.log('csv done');
+        })
+
+}
+
+// from stream
+csv()
+    .fromStream(readableStream)
+    .on('csv', (csvRow) => {
+        console.log('csvRow: ', csvRow);
+    })
+    .on('done', (error) => {
+        console.log('csv done');
+    })
